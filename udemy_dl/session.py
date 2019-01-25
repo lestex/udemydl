21ca03f2559f756b1193a674840baea5d3a2af55
Andrey Larin <lestex@gmail.com>
2019-01-25 23:34:01 +0300

    updates session

--- udemy_dl/session.py
+++ udemy_dl/session.py
@@ -1,4 +1,4 @@
-import requests.sessions
+import requests
 
 class Session:
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:39.0) Gecko/20100101 Firefox/39.0',
@@ -7,7 +7,7 @@ class Session:
                'Referer': 'https://www.udemy.com/join/login-popup'}
 
     def __init__(self):
-        self.session = requests.sessions.Session()
+        self.session = requests.Session()
         
     def set_auth_headers(self, access_token, client_id):
         self.headers['X-Udemy-Bearer-Token'] = access_token
@@ -18,6 +18,5 @@ class Session:
 
     def post(self, url, data):
         return self.session.post(url, data, headers=self.headers)
-
 
 session = Session()
