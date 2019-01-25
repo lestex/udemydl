70627519fad7f3a0984e4537c0c92c31b55d3c44
Andrey Larin <lestex@gmail.com>
2019-01-25 20:00:05 +0300

    updates setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import subprocess
+from setuptools import setup
+
+requirements = [pkg.split('=')[0] for pkg in open('requirements.txt').readlines()]
+
+description = 'Download videos from Udemy for personal offline use'
+try:
+    subprocess.call(["pandoc", "README.md", "-f", "markdown", "-t", "rst", "-o", "README.rst"])
+    long_description = open("README.rst").read()
+except OSError:
+    print("Pandoc not installed")
+    long_description = description
+
+classifiers = ['Environment :: Console',
+               'Programming Language :: Python :: 2',
+               'Programming Language :: Python :: 3',
+               'Topic :: Multimedia :: Video',]
+
+version = open('CHANGES.txt').readlines()[0][1:].strip()
+
+setup(name='udemy-dl',
+      version=version,
+      description=description,
+      author='Andrey Larin',
+      author_email='lestex@gmail.com',
+      url='https://github.com/lestex/udemy-dl',      
+      install_requires=requirements,
+      long_description=long_description,
+      packages=['udemy_dl'],
+      classifiers=classifiers,
+      entry_points={
+        'console_scripts': [
+            '{}=udemy_dl.cli:main'.format('udemy-dl')
+        ]
+      }
+    )
