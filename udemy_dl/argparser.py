c2a251783566ffc4b7fd781af61f78028ab62c12
Andrey Larin <lestex@gmail.com>
2019-04-23 12:33:54 +0300

    adds argparser

---
+++ udemy_dl/argparser.py
@@ -0,0 +1,35 @@
+import argparse
+from udemy_dl import __title__, __version__
+
+parser = argparse.ArgumentParser(
+    description='Download courses from Udemy.com',
+    prog=__title__
+)
+parser.add_argument(
+    '--debug',
+    help='Enable debug mode',
+    action='store_const',
+    const=True,
+    default=False
+)
+parser.add_argument(
+    'link',
+    help='Link for udemy course',
+    action='store'
+)
+parser.add_argument(
+    '-u', '--username',
+    help='Username / Email',
+    default=None, action='store'
+)
+parser.add_argument(
+    '-p', '--password',
+    help='Password',
+    default=None,
+    action='store'
+)
+parser.add_argument(
+    '-o', '--output',
+    help='Output directory',
+    default=None, action='store'
+)
