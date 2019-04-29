80ff088a7520f283d50c20c71dd8e522a1d9e992
Andrey Larin <lestex@gmail.com>
2019-04-29 12:22:18 +0300

    updates argparser

--- udemy_dl/argparser.py
+++ udemy_dl/argparser.py
@@ -1,43 +1,10 @@
 import argparse
 from udemy_dl import __title__, __version__
 
-parser = argparse.ArgumentParser(
-    description='Download courses from Udemy.com',
-    prog=__title__
-)
-parser.add_argument(
-    '--debug',
-    help='Enable debug mode',
-    action='store_const',
-    const=True,
-    default=False
-)
-parser.add_argument(
-    'link',
-    help='Link for udemy course',
-    action='store'
-)
-parser.add_argument(
-    '-u', '--username',
-    help='Username / Email',
-    default=None,
-    action='store'
-)
-parser.add_argument(
-    '-p', '--password',
-    help='Password',
-    default=None,
-    action='store'
-)
-parser.add_argument(
-    '-o', '--output',
-    help='Output directory',
-    default=None,
-    action='store'
-)
-parser.add_argument(
-    '-q', '--video-quality', 
-    help='Select video quality 720, 480 (default is 720)',
-    default='720',
-    action='store'
-)
+parser = argparse.ArgumentParser(description='Download courses from Udemy.com', prog=__title__)
+parser.add_argument('--debug', help='Enable debug mode', action='store_const', const=True, default=False)
+parser.add_argument('link', help='Link for udemy course', action='store')
+parser.add_argument('-u', '--username', help='Username / Email', default=None, action='store')
+parser.add_argument('-p', '--password', help='Password', default=None, action='store')
+parser.add_argument('-o', '--output', help='Output directory', default=None, action='store')
+parser.add_argument('-q', '--video-quality', help='Select video quality 720, 480 (default is 720)', default='720', action='store')
