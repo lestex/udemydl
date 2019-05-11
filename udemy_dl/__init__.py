dab548db4183eeeb832b5e5c29865b6d114ee4d5
Andrey Larin <lestex@gmail.com>
2019-05-12 00:49:53 +0300

    bumps up to 0.5.0

--- udemy_dl/__init__.py
+++ udemy_dl/__init__.py
@@ -11,6 +11,6 @@ ATTACHMENT_URL = 'https://www.udemy.com/api-2.0/users/me/subscribed-courses/{cou
 __title__ = 'udemydl'
 __author__ = 'Andrey Larin'
 __email__ = 'lestex@gmail.com'
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 logger = logging.getLogger(__title__)
